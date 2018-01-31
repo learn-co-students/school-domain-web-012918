a5d62f9ccd38ab3864cc7ce173606c379d579a3d
scott <you@example.comitsmescottb123@gmail.com>
2018-01-31 11:56:16 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
-# code here!
+class School
+
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+
+    if @roster[grade] == nil
+      @roster[grade] = [name]
+    else
+      @roster[grade] << name
+    end
+
+  end
+
+  def grade(grade)
+    @roster.each do |g, n|
+      if g == grade
+        return n
+      end
+    end
+  end
+
+  def sort
+    @roster.each do |g, n|
+      n.sort!
+    end
+  end
+end
