93fcf07e64e6ed6d98ed4543fca98915c773b3a4
porzingod <jasonlim.ny@gmail.com>
2018-01-31 15:23:49 -0500

    create School class

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+  attr_reader :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |grade, names|
+      names.sort!
+    end
+  end
+end
