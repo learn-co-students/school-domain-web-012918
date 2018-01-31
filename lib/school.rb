18d5cc8d1bfb80313487245a813a558dcf98ebcc
github username <github email address>
2018-01-31 13:45:29 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+  attr_reader :roster, :add_student
+
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def add_student(student, roster)
+    @add_student
+    unless @roster[roster]
+      @roster[roster] = []
+      @roster[roster] << student
+    else
+      @roster[roster] << student
+    end
+  end
+
+  def grade(roster)
+    @roster[roster]
+  end
+
+  def sort
+    @roster.sort_by{|grade, student_arr|
+      @roster[grade].sort!
+      grade
+    }.to_h
+  end
+end
