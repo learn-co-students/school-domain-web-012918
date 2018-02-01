f2318f354e066fe96acaf7e03fd76eb8da01cf26
github username <github email address>
2018-01-31 23:07:28 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+require 'pry'
+
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name,grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(num)
+    @roster[num]
+  end
+
+  def sort
+    new_hash = {}
+    @roster.map do |grade,students|
+      new_hash[grade] = students.sort
+    end
+    new_hash
+  end
+end
