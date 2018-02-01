9ee4f24a61ebb6b97ab8116712c720153f0492a5
github username <github email address>
2018-01-31 23:11:10 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -21,7 +21,7 @@ class School
 
   def sort
     new_hash = {}
-    @roster.map do |grade,students|
+    @roster.each do |grade,students|
       new_hash[grade] = students.sort
     end
     new_hash
