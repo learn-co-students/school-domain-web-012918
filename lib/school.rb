3309e03e14d647510c0cdeebfeac3bbed48efb7e
jaf7 <janthonyfields@gmail.com>
2018-02-01 21:15:56 -0500

    Created a domain model

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require 'pry'
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(level)
+    @roster[level]
+  end
+
+  def sort
+      @roster.each {|k,v| v.sort!}
+  end
+
+end
